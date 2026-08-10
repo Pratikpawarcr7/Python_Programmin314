4336eb7502cb6a341eb8bc564ead5afe616c4159
Pratikpawarcr7 <pawarpratikcr7@gmail.com>
2026-08-10 14:13:43 -0400

    Update Program28.py

--- Programs/Program28.py
+++ Programs/Program28.py
@@ -1,7 +1,7 @@
 import os
 def main():
 
-    os.rename("Pratik.txt","Marvellous_Infosystem.txt")
+    os.rename("Demo.txt","Marvellous_Infosystem.txt")
     
 if __name__ == "__main__":
     main()
